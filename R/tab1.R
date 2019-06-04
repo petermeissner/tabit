5d80840b97d3cfd7c1df0e8a6eb51c2d3541565f
petermeissner <retep.meissner@gmail.com>
2019-06-04 10:00:02 +0200

    init

---
+++ R/tab1.R
@@ -0,0 +1,28 @@
+#' tab1
+#'
+#' @param x
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+tab1 <-
+  function (x, ...) {
+    UseMethod("tab1", x)
+  }
+
+
+#' tab1.vector
+#'
+#' @param x
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+tab1.vector <-
+  function(x, ...){
+
+  }
